15d59c6996eabc9076a7e5af34cee81b8bb7a859
Tomer <107123466+Tomercio@users.noreply.github.com>
2024-09-03 16:16:38 +0300

    Navbar + HomePage

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+const rootElement = document.getElementById("root");
+
+const root = ReactDOM.createRoot(rootElement);
+root.render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
---
+++ frontend/src/pages/HeroSection.jsx
@@ -0,0 +1,37 @@
+import { Button, Container, Form, Row, Col } from "react-bootstrap";
+import "../styles/heroSection.css";
+
+const HeroSection = () => {
+  return (
+    <div className="homepage d-flex align-items-center justify-content-center ">
+      <div className="homepage-overlay text-center">
+        <h1 className="homepage-title">Find Your Ideal Property</h1>
+
+        <div className="button-group mb-4">
+          <button className="btn-option active">Buyer</button>
+          <button className="btn-option">Renter</button>
+          <button className="btn-option">Seller</button>
+        </div>
+
+        <Container className="search-container justify-content-center">
+          <Row className="justify-content-center">
+            <Col>
+              <Form.Control
+                type="text"
+                placeholder="Enter Address"
+                className="search-input"
+              />
+            </Col>
+            <Col xs="auto">
+              <Button variant="outline-warning" id="search">
+                Search
+              </Button>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
