7875669a7d8a62311c969e9e167599d4674fec19
Adam <adambeaton42@gmail.com>
2024-09-08 09:38:19 +0300

    add AppError functionlity

--- backend/controllers/propertyControllers.js
+++ backend/controllers/propertyControllers.js
@@ -1,18 +1,13 @@
 
 const Property = require("../models/propertyModel.js");
 const asyncHandler = require("express-async-handler");
-
-
-exports.getProperties = asyncHandler(async (req, res, next) => {
-    const properties = await Property.find()
-    console.log(properties);
-    res.status(200).json({
-        status: 'success',
-        properties
-    })
-})
+const AppError = require("./../utils/AppError");
 
 exports.addProperty = asyncHandler(async (req, res, next) => {
+    const { title, propertyType, location, price, description, area, agent } = req.body;
+    if (!title || !propertyType || !price || !description || !location || !area || !agent) {
+        return next(new AppError(400, 'Please provide all the required fields'));
+    }
     const newProperty = await Property.create(req.body);
     res.status(201).json({
         status: 'success',
@@ -20,32 +15,74 @@ exports.addProperty = asyncHandler(async (req, res, next) => {
     });
 });
 
-exports.getPropertyById = asyncHandler(async (req, res, next) => {
-    const property = await Property.findById(req.params.id);
-    if (!property) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Property not found'
-        });
+//actual getProperties---------->>>>>
+// exports.getProperties = asyncHandler(async (req, res, next) => {
+//     const properties = await Property.find();
+//     if (!properties || properties.length === 0) {
+//         return next(new AppError(404, 'No properties found'));
+//     }
+
+//     res.status(200).json({
+//         status: 'success',
+//         properties
+//     });
+// });
+
+// getProperties for debugging
+exports.getProperties = asyncHandler(async (req, res, next) => {
+    const properties = await Property.find();
+    if (!properties || properties.length === 0) {
+        return next(new AppError(404, 'No properties found'));
     }
+
+    const reorderedProperties = properties.map(property => {
+        const { _id, ...rest } = property.toObject();
+        return { _id, ...rest };
+    });
+
+    res.status(200).json({
+        status: 'success',
+        properties: reorderedProperties
+    });
+});
+
+exports.getPropertyById = asyncHandler(async (req, res, next) => {
+    const { id } = req.params;
+    if (!id) {
+        return next(new AppError(400, 'No property ID provided'));
+    }
+
+    const property = await Property.findById(id);
+    if (!property) {
+        return next(new AppError(404, "Property not found"));
+    }
+
     res.status(200).json({
         status: 'success',
         property
     });
 });
 
-exports.editPropertyById = asyncHandler(async (req, res, next) => {
 
-    const property = await Property.findByIdAndUpdate(req.params.id, req.body, {
+exports.editPropertyById = asyncHandler(async (req, res, next) => {
+    const { id } = req.params;
+    if (!id) {
+        return next(new AppError(400, 'No property ID provided'));
+    }
+
+    const { title, propertyType, price, description, location,
+        bedrooms, bathrooms, area, status, images, virtualTour } = req.body;
+    if (!title && !propertyType && !price && !description && !location && !bedrooms
+        && !bathrooms && !area && !status && !images && !virtualTour) {
+        return next(new AppError(400, 'Please provide at least one field to update'));
+    }
+
+    const property = await Property.findByIdAndUpdate(id, req.body, {
         new: true,
         runValidators: true
     });
-
     if (!property) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Property not found'
-        });
+        return next(new AppError(404, 'Property not found'));
     }
 
     res.status(200).json({
@@ -54,14 +91,16 @@ exports.editPropertyById = asyncHandler(async (req, res, next) => {
     });
 });
 
-exports.deletePropertyById = asyncHandler(async (req, res, next) => {
-    const property = await Property.findByIdAndDelete(req.params.id);
 
+exports.deletePropertyById = asyncHandler(async (req, res, next) => {
+    const { id } = req.params;
+    if (!id) {
+        return next(new AppError(400, 'No property ID provided'));
+    }
+
+    const property = await Property.findByIdAndDelete(id);
     if (!property) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Property not found'
-        });
+        return next(new AppError(404, 'Property not found'));
     }
 
     res.status(200).json({
